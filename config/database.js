76551302f2607faa82d0ac15abc7c327c2d0d132
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2023-09-09 00:36:28 -0300

    add trycach block in db connection

--- config/database.js
+++ config/database.js
@@ -4,15 +4,21 @@ dotenv.config();
 console.log(process.env.DATABASE_URL);
 console.log(process.env.DATABASE_NAME);
     let db;
-    const client = new MongoClient(process.env.DATABASE_URL, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-          }
-    });
-    await client.connect();
-    db = client.db();
+
+    try {
+        const client = new MongoClient(process.env.DATABASE_URL, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+              }
+        });
+        await client.connect();
+        db = client.db();    
+    } catch (error) {
+        console.log(error);
+    }
+    
 
 export {db};
 
